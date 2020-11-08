0c23f5dca4f132fd8dc28ad4df3417294dc3f83c
Koziolek <bjkuczynski@gmail.com>
2020-11-08 16:08:04 +0100

    fix: some examples

--- .hooks/post-commit.d/e.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hook_type=${BASH_SOURCE##*/}
-
-echo "Hello from $hook_type"
--- .hooks/pre-commit.d/e.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hook_type=${BASH_SOURCE[0]}
-
-echo "Hello from $hook_type"
